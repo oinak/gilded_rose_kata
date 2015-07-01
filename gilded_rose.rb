0697b3570224f692d0a8441da74f348bd0f38bc4
Fernando Martínez <fernando.mdelacueva@gmail.com>
2015-07-01 19:39:15 +0200

    brie specific updates extracted, tests green

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,33 +1,37 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+    if item.name == 'Aged Brie'
+      update_brie(item)
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          if item.name != 'Sulfuras, Hand of Ragnaros'
+            item.quality -= 1
           end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
+        end
+      else
+        if item.quality < 50
+          item.quality += 1
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality += 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality += 1
+              end
             end
           end
         end
       end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
+
+      if item.name != 'Sulfuras, Hand of Ragnaros'
+        item.sell_in -= 1
+      end
+
+      if item.sell_in < 0
         if item.name != 'Backstage passes to a TAFKAL80ETC concert'
           if item.quality > 0
             if item.name != 'Sulfuras, Hand of Ragnaros'
@@ -37,11 +41,21 @@ def update_quality(items)
         else
           item.quality = item.quality - item.quality
         end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
       end
+    end
+  end
+end
+
+def update_brie(item)
+  if item.quality < 50
+    item.quality += 1
+  end
+
+  item.sell_in -= 1
+
+  if item.sell_in < 0
+    if item.quality < 50
+      item.quality += 1
     end
   end
 end
